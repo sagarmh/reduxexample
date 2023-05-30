5771321ee7cd93ccf4fba0be87e4b43215798eeb
sagmhatr1 <sagar.mhatre@publicissapient.com>
2023-05-30 20:10:02 +0530

    files added

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import todosReducer from '../features/todos/todoSlice';
+
+export default configureStore({
+  reducer: {
+    todos: todosReducer,
+  },
+});
